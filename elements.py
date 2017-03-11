fac21262041bf9d2ee68aa5647d05e0ed4880a97
stevemolloy <sdmolloy@gmail.com>
2017-03-11 12:05:46 +0100

    Added CavityAnalytic class

--- elements.py
+++ elements.py
@@ -23,3 +23,30 @@ class Quad:
         s += ' B = ' + self.B.__repr__()
         s += ' aperRadius = ' + self.aperRadius.__repr__()
         return s
+
+class CavityAnalytic:
+    def __init__(self, pynacRepr):
+        cavID = int(pynacRepr[1][0][0])
+        xesln = float(pynacRepr[1][1][0])
+        phase = float(pynacRepr[1][1][1])
+        fieldReduction = float(pynacRepr[1][1][2])
+        isec = int(pynacRepr[1][1][3])
+        self.cavID = Param(val = cavID, unit = None)
+        self.xesln = Param(val = xesln, unit = 'cm')
+        self.phase = Param(val = phase, unit = 'deg')
+        self.fieldReduction = Param(val = fieldReduction, unit = 'percent')
+        self.isec = Param(val = isec, unit = None)
+
+    def adjustPhase(self, adjustment):
+        self.phase = self.phase._replace(val = self.phase.val + adjustment)
+
+    def scaleField(self, scalingFactor):
+        oldField = self.fieldReduction.val
+        newField = 100.0 * (scalingFactor * (1.0 + oldField/100.0) - 1.0)
+        self.fieldReduction = self.fieldReduction._replace(val = newField)
+
+    def dynacRepresentation(self):
+        return ['CAVMC', [
+            [self.cavID.val],
+            [self.xesln.val, self.phase.val, self.fieldReduction.val, self.isec.val, 1],
+            ]]

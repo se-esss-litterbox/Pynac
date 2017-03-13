378f929f2941611cdee0b6195124ed07d506d7b9
stevemolloy <sdmolloy@gmail.com>
2017-03-13 16:02:33 +0100

    Adding tests

---
+++ testing/test_core.py
@@ -0,0 +1,29 @@
+import unittest
+from Pynac.Core import Pynac
+
+class PynacTest(unittest.TestCase):
+    def setUp(self):
+        self.pynacInstance = Pynac('ESS_with_SC_ana.in')
+
+    def test_getPlotInds(self):
+        self.assertEqual(self.pynacInstance.getNumPlots(), 7)
+
+    def test_RemoveElement(self):
+        plotInds = self.pynacInstance.getPlotInds()
+        del self.pynacInstance.lattice[plotInds[0]]
+        self.assertEqual(self.pynacInstance.getNumPlots(), 6)
+
+    def test_getXinds_quads(self):
+        quadInds = self.pynacInstance.getXinds('QUADRUPO')
+        self.assertEqual(len(quadInds), 285)
+
+    def test_getXinds_cavmcs(self):
+        cavmcinds = self.pynacInstance.getXinds('CAVMC')
+        self.assertEqual(len(cavmcinds), 146)
+
+    def test_getXinds_nonsense(self):
+        nonsenseinds = self.pynacInstance.getXinds('BLAHBLAHBLAH')
+        self.assertEqual(len(nonsenseinds), 0)
+
+if __name__ == '__main__':
+    unittest.main()

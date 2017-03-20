e736312622d0047c7717d211373c70b5c6cf3e9c
stevemolloy <sdmolloy@gmail.com>
2017-03-20 10:34:48 +0100

    A new release!

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'Pynac',
   packages = ['Pynac'], # this must be the same as the name above
-  version = '0.9.3',
+  version = '0.9.4',
   description = 'A Python wrapper around the Dynac charged particle simulator',
   author = 'Stephen Molloy',
   author_email = 'sdmolloy@gmail.com',
   url = 'https://github.com/se-esss-litterbox/Pynac', # use the URL to the github repo
-  download_url = 'https://github.com/se-esss-litterbox/Pynac/archive/0.9.2.tar.gz',
+  download_url = 'https://github.com/se-esss-litterbox/Pynac/archive/0.9.4.tar.gz',
   keywords = ['simulation', 'particle', 'dynamics'], # arbitrary keywords
   classifiers = [],
 )

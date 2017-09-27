273a4738bc40ad21f070fe171f9146261116f72d
stevemolloy <sdmolloy@gmail.com>
2017-09-27 11:29:15 +0200

    Testing instantiation of elements from dynac input files

--- tests/test_elements.py
+++ tests/test_elements.py
@@ -29,8 +29,44 @@ class BasicElementTests(unittest.TestCase):
         SubElement()
 
 
+class ElementInstantiationTests(unittest.TestCase):
+    def setUp(self):
+        self.pynacInstance = Pynac(os.path.join(os.path.dirname(__file__), 'ESS_with_SC_ana.in'))
+
+    def test_quad_class(self):
+        quadinds = self.pynacInstance.get_x_inds('QUADRUPO')
+        quad = self.pynacInstance.lattice[quadinds[0]]
+        self.assertIsInstance(quad, ele.Quad)
+
+    def test_cavmc_class(self):
+        cavinds = self.pynacInstance.get_x_inds('CAVMC')
+        cav = self.pynacInstance.lattice[cavinds[0]]
+        self.assertIsInstance(cav, ele.CavityAnalytic)
+
+    def test_drift_class(self):
+        driftinds = self.pynacInstance.get_x_inds('DRIFT')
+        drift = self.pynacInstance.lattice[driftinds[0]]
+        self.assertIsInstance(drift, ele.Drift)
+
+    def test_cavsc_class(self):
+        cavscinds = self.pynacInstance.get_x_inds('CAVSC')
+        cav = self.pynacInstance.lattice[cavscinds[0]]
+        self.assertIsInstance(cav, ele.AccGap)
+
+    def test_reject_class(self):
+        aperinds = self.pynacInstance.get_x_inds('REJECT')
+        aper = self.pynacInstance.lattice[aperinds[0]]
+        self.assertIsInstance(aper, ele.Set4DAperture)
+
+    def test_buncher_class(self):
+        buncherinds = self.pynacInstance.get_x_inds('BUNCHER')
+        buncher = self.pynacInstance.lattice[buncherinds[0]]
+        self.assertIsInstance(buncher, ele.Buncher)
+
+
 class ElementManipulationTest(unittest.TestCase):
-    pynacInstance = Pynac(os.path.join(os.path.dirname(__file__), 'ESS_with_SC_ana.in'))
+    def setUp(self):
+        self.pynacInstance = Pynac(os.path.join(os.path.dirname(__file__), 'ESS_with_SC_ana.in'))
 
     def test_scaleQuad(self):
         quadinds = self.pynacInstance.get_x_inds('QUADRUPO')
@@ -38,13 +74,13 @@ class ElementManipulationTest(unittest.TestCase):
         quad.scaleField(0)
         self.assertEqual(quad.B.val, 0)
 
-    def test_scaleCav(self):
+    def test_scaleCavMC(self):
         cavinds = self.pynacInstance.get_x_inds('CAVMC')
         cav = self.pynacInstance.lattice[cavinds[0]]
         cav.scaleField(0)
         self.assertEqual(cav.fieldReduction.val, -100)
 
-    def test_adjustPhase(self):
+    def test_adjustCAVMCPhase(self):
         adjustBy = 10
         cavinds = self.pynacInstance.get_x_inds('CAVMC')
         cav = self.pynacInstance.lattice[cavinds[0]]
